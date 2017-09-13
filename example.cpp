8fc71cb12845813baa24a3d48e74678607a46217
buffereditor <31925085+buffereditor@users.noreply.github.com>
2017-09-13 15:32:26 +0300

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+// Class Declaration
+
+class person {
+    
+public:
+    string name;
+    int number;
+};
+
+//Main Function
+
+int main() {
+    // Object Creation For Class
+    person obj;
+
+    //Get Input Values For Object Variables
+    cout << "Enter Name :";
+    cin >> obj.name;
+
+    cout << "Enter Number :";
+    cin >> obj.number;
+
+    //Show the Output
+    cout << obj.name << ": " << obj.number << endl;
+
+    getch();
+    return 0;
+}
